b8ad0275b5cec1fd29d335e6d4de76da6d6e9e4d
luke-ded <luke@dederich.org>
2025-04-16 16:53:55 -0400

    Coming soon message

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -4,8 +4,11 @@ function HomePage()
 {
 
     return(
-        <div className="h-screen w-screen">
+        <div className="flex-col h-screen w-screen items-center justify-center">
             <NavBar />
+            <div className="flex w-screen h-13/14 items-center justify-center bg-gradient-to-b from-black/60 to-transparent">
+                <h1 className="text-black text-6xl font-bold">Coming soon... an interactive walking map of UCF.</h1>
+            </div>
         </div>
     );
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,9 +4,9 @@ function NavBar()
 {
 
     return(
-        <div className="flex h-1/12 bg-black/90 border-b-2 border-[#242424]">
+        <div className="flex h-1/14 bg-black/90 border-b border-[#ffcc00]">
             <div className="flex items-center ml-3">
-                <img className="h-7/10 w-auto" src={src} alt="UCF Logo" />
+                <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl text-[#ffca09] ml-3">walkUCF</h1>
             </div>
         </div>
