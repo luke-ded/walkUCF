24be43cf82b29380a5eb37b23e5d93e9805496d7
luke-ded <luke@dederich.org>
2025-04-16 11:00:22 -0400

    Beckground mockup

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ function NavBar()
 {
 
     return(
-        <div className="flex h-1/12 bg-[#242424] border-b-2 border-black">
+        <div className="flex h-1/12 bg-black/90 border-b-2 border-[#242424]">
             <div className="flex items-center ml-2">
                 <img className="h-7/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl text-[#ffca09] ml-5">walkUCF</h1>
