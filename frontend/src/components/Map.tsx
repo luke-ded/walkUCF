237c9ede07187cc1b3cbd386782cfa68f129d02f
luke-ded <luke@dederich.org>
2025-04-17 14:54:01 -0400

    Map zoom and bounds

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,5 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup/*, Polyline */} from 'react-leaflet';
-import {LatLngTuple/*, LatLngExpression*/} from 'leaflet';
+import {LatLngTuple, LatLngBoundsExpression/*, LatLngExpression*/} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 
@@ -12,15 +12,21 @@ function Map()
         [51.52, -0.12],
       ]; */
 
+      const bounds: LatLngBoundsExpression = [
+        [28.590814194772776, -81.20725051378662], // Southwest corner
+        [28.611654822136117, -81.18757949435675]  // Northeast corner
+      ];
+
       return (
         <div className="h-full w-full rounded shadow-lg">
-          <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="h-full w-full z-0">
+          <MapContainer center={position} zoom={16} minZoom={15} maxZoom={18} scrollWheelZoom={true} 
+            maxBounds={bounds} maxBoundsViscosity={1} className="h-full w-full z-0">
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={position}>
-              <Popup>Hello from London!</Popup>
+              <Popup>UCF!</Popup>
             </Marker>
             {/* <Polyline positions={path} color="blue" /> */}
           </MapContainer>
