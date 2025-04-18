fae06b4dee2a9b30a2a2d7e40b0eebdd49cbcccb
luke-ded <luke@dederich.org>
2025-04-18 14:00:53 -0400

    Search Refactor

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,6 +1,6 @@
 import NavBar from "./components/NavBar";
 import Map from "./components/Map.tsx";
-import SearchBar from "./components/SearchBar.tsx";
+import Search from "./components/Search.tsx";
 
 function HomePage()
 {
@@ -9,7 +9,7 @@ function HomePage()
             <NavBar />
             <div className="flex w-screen h-13/14 items-center justify-center bg-gradient-to-b from-black/60 to-transparent">
                 <div className="flex flex-col items-center justify-start w-2/5 h-4/5 mr-20">
-                    <SearchBar />
+                    <Search />
                     <h1 className="text-black mt-5 text-2xl font-bold">Coming soon... an interactive walking map of UCF.</h1>
                 </div>
                 <div className="w-2/5 h-4/5 border-2 border-[#ffca09] rounded-md">
