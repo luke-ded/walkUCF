2e4fd297bf330f64b54d6d7bca0515d0d49dc6ba
luke-ded <luke@dederich.org>
2025-04-18 21:25:40 -0400

    Page title update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar()
         <div className="flex h-1/14 bg-black/90 border-b border-[#ffcc00]">
             <div className="flex items-center ml-3">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
-                <h1 className="text-3xl text-[#ffca09] ml-3">walkUCF</h1>
+                <h1 className="text-3xl text-[#ffca09] font-semibold ml-3">walkUCF</h1>
             </div>
         </div>
     );
