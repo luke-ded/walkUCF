bbc8620e221590addc6d43272463117033079cdd
luke-ded <luke@dederich.org>
2025-04-19 11:40:48 -0400

    Parameter checkboxes

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -13,8 +13,18 @@ function HomePage()
                     <Search />
                     <RouteList />
                 </div>
-                <div className="w-2/5 h-4/5 border-2 border-[#ffca09] rounded-md shadow-lg">
-                    <Map />
+                <div className="flex flex-col justify-between w-2/5 h-4/5 border-2 border-[#ffca09] rounded-md shadow-lg">
+                    <div className="w-full h-38/40 self-start border-b-2 border-[#ffca09] rounded-md">
+                        <Map />
+                    </div>
+                    <div className="flex w-full h-2/40 bg-black/25 justify-center items-center">
+                        <input type="checkbox" value="" className="w-5 h-5 bg-neutral-100 border-neutral-300 rounded-lg" />
+                        <h1 className="ml-1 test-neutral-200">Cutting through buildings</h1>
+                        <input type="checkbox" value="" className="w-5 h-5 ml-4 bg-neutral-100 border-neutral-300 rounded-lg" />
+                        <h1 className="ml-1 test-neutral-200">Jaywalking</h1>
+                        <input type="checkbox" value="" className="w-5 h-5 ml-4 bg-neutral-100 border-neutral-300 rounded-lg" />
+                        <h1 className="ml-1 test-neutral-200">Cutting across grass</h1>
+                    </div>
                 </div>
             </div>
         </div>
