5589220b6a1de2a01878afc4907a45b782b36917
luke-ded <luke@dederich.org>
2025-04-25 14:20:36 -0400

    Default checking

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -26,7 +26,7 @@ function HomePage()
                         <Map />
                     </div>
                     <div className="flex w-full h-2/40 bg-black/25 justify-center items-center">
-                        <input type="checkbox" value="" className="w-5 h-5 bg-neutral-100 border-neutral-300 rounded-lg" />
+                        <input type="checkbox" value="" className="w-5 h-5 bg-neutral-100 border-neutral-300 rounded-lg" checked/>
                         <h1 className="ml-1 test-neutral-200">Cutting through buildings</h1>
                         <input type="checkbox" value="" className="w-5 h-5 ml-4 bg-neutral-100 border-neutral-300 rounded-lg" />
                         <h1 className="ml-1 test-neutral-200">Jaywalking</h1>
