bd43f04df935f50c5220927fa99caa625cfcf4fd
luke-ded <luke@dederich.org>
2025-04-24 20:56:38 -0400

    About button

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,10 +4,13 @@ function NavBar()
 {
 
     return(
-        <div className="flex h-1/14 bg-black/90 border-b border-[#ffcc00]">
+        <div className="flex h-1/14 bg-black/90 border-b justify-between border-[#ffcc00]">
             <div className="flex items-center ml-3">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl text-[#ffca09] font-semibold ml-3">walkUCF</h1>
+            </div>
+            <div className="flex items-center mr-3">
+                <button className="cursor-pointer hover:bg-[#ffe68c]/20 p-2 border-1 border-[#ffe68c]/30 rounded-xl">About</button>
             </div>
         </div>
     );
