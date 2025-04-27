797903e63e63228bc9fa0c12e17ce0895dedfa6c
luke-ded <luke@dederich.org>
2025-04-27 18:58:13 -0400

    Navbar button updates

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -18,15 +18,15 @@ const NavBar: React.FC<ChildProps> = ({ toggleAbout, about }) =>
     }
 
     return(
-        <div className="flex h-1/14 bg-black/90 border-b justify-between border-[#ffcc00]">
+        <div className="flex h-1/14 bg-black/90 border-b-2 border-[#ffcc00] justify-between">
             <div className="flex items-center ml-3">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl text-[#ffca09] font-semibold ml-3">walkUCF</h1>
             </div>
             <div className="flex items-center mr-3">
-            <button className="cursor-pointer hover:bg-[#ffe68c]/20 p-2 mr-3 border-1 border-[#ffe68c]/30 rounded-xl" onClick={()=> darkModeHandler()}>
+                <button className="cursor-pointer hover:bg-[#ffe68c]/20 px-2 mr-3 py-1.5 border-2 border-[#ffe68c]/30 rounded-xl" onClick={()=> darkModeHandler()}>
                 Color Mode</button>
-                <button className="cursor-pointer hover:bg-[#ffe68c]/20 p-2 border-1 border-[#ffe68c]/30 rounded-xl"
+                <button className="cursor-pointer hover:bg-[#ffe68c]/20 px-2 py-1.5 border-2 border-[#ffe68c]/30 rounded-xl"
                 onClick={() => toggleAbout(!about)}>About</button>
             </div>
         </div>
