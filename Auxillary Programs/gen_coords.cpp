ca2b1e1d10b1a24fff95d3140f215433fad36f78
luke-ded <luke@dederich.org>
2025-05-02 10:07:00 -0400

    Generate coordinates from osm

--- Auxillary Programs/gen_coords.cpp
+++ Auxillary Programs/gen_coords.cpp
@@ -1,42 +1,87 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<regex>
+#include<iomanip>
 
 using namespace std;
 
 int main(void)
 {
-    ifstream inputfile("Text Files/geojson apollo circuit in, athletic, lake claire.txt");
-    string line, x, y;
+    cout<<"Program running..."<<endl;
 
-    ofstream outputfile("Text Files/coords.txt");
+    ifstream inputfile("Auxillary Files/UCFmap.osm");
+    ofstream outputfile("Auxillary Files/coords.txt");
 
-    int i = 1;
-    while (getline(inputfile, line)) {
+    string line, newline;
+    long long id;
+    double lat, lon;
 
-        //cout<<line<<"and "<<"\t\t\"coordinates\": [\n";
-        if(line.find("\"coordinates\": [") != string::npos)
+
+    while (getline(inputfile, line)) 
+    {
+        if(line.find("<node id") != string::npos)
         {
-            cout<<i<<"\n";
-            getline(inputfile, line);
-            x = line.erase(line.length() - 2, line.length() - 1);
+            regex delimiter(" "); 
+            vector<string> result(sregex_token_iterator(line.begin(), line.end(), delimiter, -1), sregex_token_iterator());
+            
+            if(result.size() != 11)
+            {
+                cout<<"Invalid line: "<<line<<endl;
+                return 0;
+            }
 
-            getline(inputfile, line);
-            y = line.erase(line.length() - 1);
+            newline = result[2];
+            newline = newline.erase(newline.length() - 1, newline.length());
+            newline = newline.erase(0, 4);
+            try
+            {
+                id = stoll(newline);
+            } catch(...)
+            {
+                cout<<"Failed at line: "<<line<<endl;
+                cout<<"Attempted to convert to integer: "<<newline<<endl;
+                return 0;
+            }
+            
+            
+            newline = result[9];
+            newline = newline.erase(newline.length() - 1, newline.length());
+            newline = newline.erase(0, 5);
+            try
+            {
+                lat = stod(newline);
+            } catch(...)
+            {
+                cout<<"Failed at line: "<<line<<endl;
+                cout<<"Attempted to convert to double: "<<newline<<endl;
+                return 0;
+            }
+
+            newline = result[10];
+            newline = newline.erase(newline.length() - 4, newline.length());
+            newline = newline.erase(0, 5);
+            try
+            {
+                lon = stod(newline);
+            } catch(...)
+            {
+                cout<<"Failed at line: "<<line<<endl;
+                cout<<"Attempted to convert to double: "<<newline<<endl;
+                return 0;
+            }
+
+            cout<<setprecision(7)<<fixed<<id<<", "<<lat<<", "<<lon<<endl;
 
             //{ id: 'A', lat: 37.7749, lon: -122.4194 }
-            outputfile<<"{id: "<<i<<" , lat: "<<y<<" , lon: "<<x<<" }\n";
-
-            i++;
+            outputfile<<setprecision(7)<<fixed<<"{id: "<<id<<" , lat: "<<lat<<" , lon: "<<lon<<" }\n";
         }
+    }
+      
+    cout<<"Program completed."<<endl;;
 
-        // Output the text from the file
-        //cout << line<<endl;
-      }
-  
-      // Close the file
-      inputfile.close();
-      outputfile.close();
+    inputfile.close();
+    outputfile.close();
 
     return 0;
 }
