e9a1589e180e2c1749a5be20c220155d21531eeb
luke-ded <luke@dederich.org>
2025-05-05 22:56:28 -0400

    Add points program

---
+++ Auxillary Programs/add_points.cpp
@@ -0,0 +1,49 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<regex>
+#include<cmath>
+#include<map>
+#include<iomanip>
+
+using namespace std;
+
+
+int main(void)
+{
+    cout<<"Please wait while the program is set up...\n";
+
+    ofstream outputfile("Auxillary Files/coords.json", ios::app);
+
+    string line;
+
+
+    cout<<"\nProgram ready. Add points in the format \"id lat lon\". Enter -1 when finished.\n";
+
+    cout<<"Add point: ";
+
+    long long id, numpoints = 0;
+    double lat, lon;
+
+    cin>>id>>lat>>lon;
+
+    while(id != -1)
+    {
+        outputfile<<setprecision(7)<<fixed<<"{\"id\": "<<id<<" , \"lat\": "<<lat<<" , \"lon\": "<<lon<<" },\n";
+        numpoints++;
+
+        cout<<"Add point: ";
+        cin>>id>>lat>>lon;;
+    }
+
+    
+    cout<<"You added "<<numpoints<<" points.\n";
+
+    // Close the file
+    outputfile.close();
+
+    return 0;
+}
