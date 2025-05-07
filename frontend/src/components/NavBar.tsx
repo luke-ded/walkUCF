299330c0d42e02ea618a83dd3b74a7202cb602e5
luke-ded <luke@dederich.org>
2025-05-06 22:10:36 -0400

    Settings navbar button

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import src from "../assets/logo.png"
 import { CiDark } from "react-icons/ci";
 import { CiLight } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
 import { useState } from "react";
 
 interface ChildProps 
@@ -26,8 +27,13 @@ const NavBar: React.FC<ChildProps> = ({ toggleAbout, about }) =>
                 <h1 className="text-3xl text-neutral-200 font-semibold ml-3">walkUCF</h1>
             </div>
             <div className="flex items-center mr-3 text-neutral-200">
+                <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" 
+                onClick={()=> darkModeHandler()}>
+                    <CiSettings size={26} />
+                </div>
                 {dark &&(
-                    <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" onClick={()=> darkModeHandler()}>
+                    <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" 
+                    onClick={()=> darkModeHandler()}>
                         <CiDark size={26} />
                     </div>
                 )}
