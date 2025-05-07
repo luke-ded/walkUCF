4d145f7f62ee233d5208d94bc76d10398e862caf
luke-ded <luke@dederich.org>
2025-05-06 22:11:44 -0400

    Settings component

---
+++ frontend/src/components/Settings.tsx
@@ -0,0 +1,15 @@
+
+function Settings()
+{
+    return (
+        <div className="absolute z-10 bg-[url(./assets/backgroundmap2.jpg)] border-2 dark:border-[#ffca09] border-[#a48100] rounded-md w-3/10 h-5/10 shadow-lg">
+            <div className="flex-col justify-center w-full h-full dark:bg-black/80 bg-[#d6d4d4]/80 rounded-sm">
+                <div className="flex justify-center items-center h-1/8 border-b-2 dark:border-[#ffca09] border-[#a48100]">
+                    <h1 className="text-2xl dark:text-neutral-200 text-neutral-700 font-bold">Settings</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
