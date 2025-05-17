940721d895448a30c27a20cb9956b6be32310151
luke-ded <luke@dederich.org>
2025-05-17 16:00:53 -0400

    Mobile improvements

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -39,7 +39,7 @@ const NavBar: React.FC<ChildProps> = ({ toggleAbout, about, toggleSettings, sett
         <div className="flex h-1/14 w-full max-w-full bg-black/90 border-b-2 border-[#ffca09] justify-between">
             <div className="flex items-center ml-3">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
-                <h1 className="text-3xl text-neutral-200 font-semibold ml-3">walkUCF</h1>
+                <h1 className="text-3xl max-sm:text-2xl text-neutral-200 font-semibold ml-3">walkUCF</h1>
             </div>
             <div className="flex items-center mr-3 text-neutral-200">
                 {dark &&(
