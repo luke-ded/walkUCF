4fe1372ec01bba8f76f522138e3d17f1572ba625
luke-ded <luke@dederich.org>
2025-05-20 14:39:54 -0400

    Safari bug test

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC<ChildProps> = ({toggleAbout}) =>
 
     return (
         <div className="absolute z-12 top-1/14 max-lg:top-1/21 left-0 w-full max-w-full h-full max-lg:20/21 flex items-center justify-center bg-black/50">
-            <div className="absolute z-12 top-1/8 max-lg:top-1/16  bg-[url(./assets/backgroundmap.jpg)] border-2 dark:border-[#ffca09] border-[#a48100] rounded-md lg:w-3/10 h-min max-sm:w-9/10 md:w-5/10 shadow-lg">
+            <div className="absolute z-12 top-1/8 max-lg:top-1/16  bg-[url(./assets/backgroundmap.jpg)] border-2 dark:border-[#ffca09] border-[#a48100] rounded-md lg:w-3/10 h-11/20 max-lg:h-3/10 max-sm:w-9/10 md:w-5/10 shadow-lg">
                 <div className="flex flex-col justify-center w-full h-full dark:bg-black/80 bg-[#d6d4d4]/80 rounded-sm">
                     <div className="flex justify-center items-center h-1/8 border-b-2 dark:border-[#ffca09] border-[#a48100]">
                         <h1 className="text-2xl py-1 dark:text-neutral-200 text-neutral-700 font-bold">About this Project</h1>
--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -83,7 +83,7 @@ const Settings: React.FC<ChildProps> = ({triggerRerender, toggleSettings, settin
 
     return (
         <div className="absolute z-12 top-1/14 max-lg:top-1/21 left-0 w-full max-w-full h-full max-lg:20/21 flex items-center justify-center bg-black/50">
-            <div className="absolute z-12 top-1/8 max-lg:top-1/16 bg-[url(./assets/backgroundmap.jpg)] border-2 dark:border-[#ffca09] border-[#a48100] rounded-md lg:w-3/10 h-min max-sm:w-9/10 md:w-5/10 shadow-lg">
+            <div className="absolute z-12 top-1/8 max-lg:top-1/16 bg-[url(./assets/backgroundmap.jpg)] border-2 dark:border-[#ffca09] border-[#a48100] rounded-md lg:w-3/10 h-7/20 max-lg:h-3/10 max-sm:w-9/10 md:w-5/10 shadow-lg">
                 <div className="flex-col justify-center w-full h-full dark:bg-black/80 bg-[#d6d4d4]/80 rounded-sm">
                     <div className="flex justify-center items-center h-1/8 border-b-2 dark:border-[#ffca09] border-[#a48100]">
                         <h1 className="text-2xl py-1 dark:text-neutral-200 text-neutral-700 font-bold">Settings</h1>
@@ -134,7 +134,7 @@ const Settings: React.FC<ChildProps> = ({triggerRerender, toggleSettings, settin
                             </div>
                         </div> */}
                     </div>
-                    <div className="flex w-full h-2/8 mt-5 justify-center items-center">
+                    <div className="flex w-full h-2/8 justify-center items-center">
                         <button className="cursor-pointer h-10 mb-5 hover:bg-[#ffe68c]/20 dark:bg-black/40 bg-white/70 px-2 py-1.5 border-2 dark:border-[#ffe68c] border-[#a48100] rounded-xl mr-3"
                         onClick={save}>Save</button>
                         <button className="cursor-pointer h-10 mb-5 hover:bg-[#ffe68c]/20 dark:bg-black/40 bg-white/70 px-2 py-1.5 border-2 dark:border-[#ffe68c] border-[#a48100] rounded-xl"
