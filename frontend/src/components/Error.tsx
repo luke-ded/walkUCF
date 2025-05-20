286f7fa090a91b8418d2b04cdda494b6d5e41db3
luke-ded <luke@dederich.org>
2025-05-20 11:25:51 -0400

    Error message rephrase

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -16,7 +16,7 @@ const Error: React.FC<ChildProps> = ({toggleError}) =>
                 <div className="flex-col justify-center w-full h-full dark:bg-black/80 bg-[#d6d4d4]/80 rounded-sm">
                     <div className="flex-col justify-center items-center max-sm:text-sm text-md dark:text-neutral-200 text-neutral-700 text-center h-6/8 p-3">
                         <p className="mt-3">Locations inacessible to each other. Some entrances could be inacessible depending on your map options. </p>
-                        <p className="mt-3">If this is probably incorrect, please submit a bug report <a href="https://forms.gle/XmwzZMkAw9f15xzs6" target="_blank" rel="noopener noreferrer" className="inline font-bold dark:text-red-400 text-red-500 dark:hover:text-red-300 hover:text-red-700">here</a> with the names & entrances of these two locations.</p>
+                        <p className="mt-3">If this is probably incorrect, please submit a issue report <a href="https://forms.gle/XmwzZMkAw9f15xzs6" target="_blank" rel="noopener noreferrer" className="inline font-bold dark:text-red-400 text-red-500 dark:hover:text-red-300 hover:text-red-700">here</a> with the names & entrances of these two locations.</p>
                     </div>
                     <div className="flex w-full h-2/8 items-center justify-center justify-self-end">
                         <button className="cursor-pointer h-10 hover:bg-red-400/20 dark:bg-black/40 bg-white/70 px-2 py-1.5 border-2 dark:border-red-400/75 border-red-500/75 rounded-xl"
