eee0207cc275ab15b88ff45c0ef8354c6317f20c
luke-ded <luke@dederich.org>
2025-05-20 12:27:31 -0400

    Navbar updates

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -36,30 +36,24 @@ const NavBar: React.FC<ChildProps> = ({ toggleAbout, about, toggleSettings, sett
     }
 
     return(
-        <div className="flex h-1/14 w-full max-w-full bg-black/90 border-b-2 border-[#ffca09] justify-between">
-            <div className="flex items-center ml-3">
+        <div className="flex h-1/14 min-h-[50px] w-full max-w-full px-3 bg-black/90 border-b-2 border-[#ffca09] justify-between">
+            <div className="flex items-center">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl max-sm:text-2xl text-neutral-200 font-semibold ml-3">walkUCF</h1>
             </div>
             <div className="flex items-center mr-3 text-neutral-200">
-                {dark &&(
-                    <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" 
-                    onClick={()=> darkModeHandler()}>
-                        <CiLight size={26} />
-                    </div>
-                )}
-                {!dark &&(
-                    <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" onClick={()=> darkModeHandler()}>
-                        <CiDark size={26} />
-                    </div>
-                )}
-                <div className="flex items-center justify-center cursor-pointer h-10 w-10 hover:bg-[#ffe68c]/20 mr-3 border-2 border-[#ffe68c]/30 rounded-xl" 
-                onClick={() => settingsHandler()}>
-                    <CiSettings size={26} />
+                <div className="flex items-center justify-center cursor-pointer sm:h-10 h-8 sm:w-10 w-8 hover:bg-[#ffe68c]/20 sm:mr-3 mr-2 border-2 border-[#ffe68c]/30 rounded-xl" 
+                onClick={()=> darkModeHandler()}>
+                    {dark && (<CiLight size={window.innerWidth > 640 ? 26 : 20} />)}
+                    {!dark &&(<CiDark size={window.innerWidth > 640 ? 26 : 20} />)}
                 </div>
-                <div className="cursor-pointer h-10 hover:bg-[#ffe68c]/20 px-2 py-1.5 border-2 border-[#ffe68c]/30 rounded-xl"
+                <div className="flex items-center justify-center cursor-pointer sm:h-10 h-8 sm:w-10 w-8 hover:bg-[#ffe68c]/20 sm:mr-3 mr-2 border-2 border-[#ffe68c]/30 rounded-xl" 
+                onClick={() => settingsHandler()}>
+                    <CiSettings size={window.innerWidth > 640 ? 26 : 20} />
+                </div>
+                <div className="flex items-center justify-center cursor-pointer sm:h-10 h-8 sm:w-10 w-8 hover:bg-[#ffe68c]/20 border-2 border-[#ffe68c]/30 rounded-xl"
                 onClick={() => aboutHandler()}>
-                    <CiCircleInfo size={26} />
+                    <CiCircleInfo size={window.innerWidth > 640 ? 26 : 20} />
                 </div>
             </div>
         </div>
