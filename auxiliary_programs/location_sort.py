1f135fca335ea46e9d4d500b9542e883fce89277
luke-ded <luke@dederich.org>
2025-06-02 15:01:11 -0400

    Location sort program

---
+++ auxiliary_programs/location_sort.py
@@ -0,0 +1,22 @@
+import json
+
+
+with open("./auxiliary_files/locations.json", "r") as f:
+    data = f.read()
+
+f.close()
+
+
+data = json.loads(data)
+data.sort(key=lambda entry: entry['Name'])
+
+i = 1
+for entry in data:
+    entry['key'] = i
+    i += 1
+
+
+with open("./auxiliary_files/locations.json", "w") as f:
+    f.write(json.dumps(data, indent = 2))
+
+f.close()
