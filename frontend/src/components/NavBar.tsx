2a2c2be1b5768614afdd4f42eb83b16d8d2543f7
luke-ded <luke@dederich.org>
2025-08-17 15:00:11 -0400

    Navbar color update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -36,7 +36,7 @@ const NavBar: React.FC<ChildProps> = ({ toggleAbout, about, toggleSettings, sett
     }
 
     return(
-        <div className="flex lg:h-1/14 h-1/21 w-full max-w-full px-3 bg-black/80 border-b-2 border-[#ffca09] justify-between">
+        <div className="flex lg:h-1/14 h-1/21 w-full max-w-full px-3 bg-black/85 border-b-2 border-[#ffca09] justify-between">
             <div className="flex items-center">
                 <img className="h-6/10 w-auto" src={src} alt="UCF Logo" />
                 <h1 className="text-3xl max-sm:text-2xl text-neutral-200 font-semibold ml-3">walkUCF</h1>
